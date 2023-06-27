4e32cb3d851b44f2248d18d157d09a17b991d376
Bruno Fazio <brunofazio466@gmail.com>
2023-06-27 19:22:17 -0300

    responsive para el contenido no funciona

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-     document.querySelector(".nav-menu").classList.toggle("show");
-  });
+
   
   ScrollReveal().reveal('.showcase');
   ScrollReveal().reveal('.news-cards', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
 
-/* let primerasImagenes = document.querySelector(".primeras-imagenes")
-primerasImagenes.addEventListener("click", () =>{
-    var ancho = primerasImagenes.width * 2;
-  var alto = primerasImagenes.height * 2;
-  primerasImagenes.style.width = ancho + "px";
-  primerasImagenes.style.height = alto + "px"; 
-}); */
