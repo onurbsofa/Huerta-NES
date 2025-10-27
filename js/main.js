f876a3942e065f52c02c33549c52f6f9436f03b3
Bruno <62848203+onurbsofa@users.noreply.github.com>
2025-10-27 19:13:52 -0300

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
+// Simple menu toggle for mobile
 document.querySelector(".menu-btn").addEventListener("click", () => {
      document.querySelector(".nav-menu").classList.toggle("show");
-  });
-  
-  ScrollReveal().reveal('.showcase');
-  ScrollReveal().reveal('.news-cards', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+   });
 
-/* let primerasImagenes = document.querySelector(".primeras-imagenes")
-primerasImagenes.addEventListener("click", () =>{
-    var ancho = primerasImagenes.width * 2;
-  var alto = primerasImagenes.height * 2;
-  primerasImagenes.style.width = ancho + "px";
-  primerasImagenes.style.height = alto + "px"; 
-}); */
+// Scroll reveal animations
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.portfolio-screenshot', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
